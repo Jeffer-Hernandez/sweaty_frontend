cba9e52bace8473459157ed4f8e4dca996db37e6
github username <jhernandez1692@gmail.com>
2021-02-20 18:06:23 -0500

    Troubleshooting.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("in index.js");
+const endPoint = "http://localhost:3000/api/v1/boards"
+
+document.addEventListener('DOMContentLoaded', () =>{
+    fetch(endPoint)
+})
+
+// function getBoards(){
+//     fetch(endPoint)
+//     then(response => response.json())
+    // then(boards => {
+    //     console.log(boards);
+    //     boards.data.forEach(board => {
+    //         const boardMarkup = `
+    //           <li>
+    //             <h3>${board.title}
+    //               <button>edit</button>
+    //             </h3>
+    //           </li>`;
+      
+    // document.querySelector('#board-container').innerHTML += boardMarkup;})
+    // });
+    // };
