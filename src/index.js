1c07a04ede6396e108541f941ff9617a03f30892
github username <jhernandez1692@gmail.com>
2021-02-21 17:45:45 -0500

    Created FormHandler and GET req.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,48 @@
-const endPoint = "http://localhost:3000/api/v1/boards";
+const endPoint = "http://localhost:3000/api/v1/comments";
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log("DOM is Loaded")
-    getBoards()
+    getDiscussions()
+
+    const createCommentForm = document.querySelector("#create-comment-form")
+
+    createCommentForm.addEventListener("submit", (e) => createFormHandler(e))
+
 });
 
 
-// function getBoards() {
-//   fetch(endPoint)
-//     .then(res => res.json())
-//     .then(json => console.log(json));
-// }
-
-function getBoards() {
+function getDiscussions() {
     fetch(endPoint)
       .then(res => res.json())
       .then(json => {
         // remember our JSON data is a bit nested due to our serializer
-        json.data.forEach(board => {
+        json.data.forEach(comment => {
           // double check how your data is nested in the console so you can successfully access the attributes of each individual object
-          const boardMarkup = `
-            <div data-id=${board.id}>
-              <h3>${board.attributes.title}</h3>
-            </div>
-            <br><br>`;
+          const discussionMarkup = `
+          <div data-id=${comment.id}>
+          <h3>from: ${comment.attributes.user.name}</h3>
+          <h3>discussion: ${comment.attributes.discussion.title}</h3>
+          <h3>comments:${comment.attributes.content}</h3>
+          
+          </div>
+          <br><br>`;
   
-            document.querySelector('#board-container').innerHTML += boardMarkup
+            document.querySelector('#discussion-container').innerHTML += discussionMarkup
         })
       })
-  }
+}
+
+function createFormHandler(e){
+  e.preventDefault()
+
+  const contentInput = document.querySelector("#input-content").value
+  const discussionId = parseInt(document.querySelector("#discussions").value)
+  const userId = parseInt(document.querySelector("#users").value)
+
+  postFetch(contentInput, discussionId, userId)
+
+}
+
+function postFetch(content_input, discussion_id, user_id){
+
+}
