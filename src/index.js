c19f040977b0cf27e5afec5706b33e9e41d1b5b9
github username <jhernandez1692@gmail.com>
2021-02-20 19:02:22 -0500

    Change file structure.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-const endPoint = "http://localhost:3000/api/v1/boards"
+// const uri = "http://localhost:3000/api/v1/boards"
+
+// document.addEventListener('DOMContentLoaded', () =>{
+//     fetch(uri)
+//     .then(response => response.json())
+//     .then(boards_json =>{
+//         console.log(boards_json);
+//     })
+// })
 
 document.addEventListener('DOMContentLoaded', () =>{
-    fetch(endPoint)
+    console.log("From index.js")
 })
-
-// function getBoards(){
-//     fetch(endPoint)
-//     then(response => response.json())
-    // then(boards => {
-    //     console.log(boards);
-    //     boards.data.forEach(board => {
-    //         const boardMarkup = `
-    //           <li>
-    //             <h3>${board.title}
-    //               <button>edit</button>
-    //             </h3>
-    //           </li>`;
-      
-    // document.querySelector('#board-container').innerHTML += boardMarkup;})
-    // });
-    // };
