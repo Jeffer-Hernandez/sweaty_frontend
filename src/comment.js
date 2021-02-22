db3236cd6ab5b1c367afcfa2545c677a8965a2bd
github username <jhernandez1692@gmail.com>
2021-02-22 18:55:39 -0500

    moved and refactored render()

--- src/comment.js
+++ src/comment.js
@@ -6,5 +6,24 @@ class Comment {
         this.content = commentAttributes.content
         this.user = commentAttributes.user
         this.discussion = commentAttributes.discussion
+        Comment.all.push(this)
     }
+
+    render(){
+        console.log(this)
+        return  `
+                <div data-id=${this.id}>
+                <h3>from: ${this.user.name}</h3>
+                <h3>discussion: ${this.discussion.title}</h3>
+                <h3>comment:${this.content}</h3>
+                
+                </div>
+                <br><br>`;  
+    }
+
+
+
+
 }
+
+Comment.all = []
