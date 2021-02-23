90b4db82a1936a3af31908fb7a6a67ae1f0124b4
github username <jhernandez1692@gmail.com>
2021-02-22 19:22:28 -0500

    create an edit form to render.

--- src/comment.js
+++ src/comment.js
@@ -22,8 +22,29 @@ class Comment {
     }
 
 
+    renderUpdateForm() {
+        return `
+        <form data-id=${this.id} >
+          <h3>Create a Comment!</h3>
+          <br><br>
+          
+          <textarea id='input-content' name="content" rows="8" cols="80" value="" placeholder="What's on your mind?"></textarea>
+          <br><br>
 
-
+    
+          <label>Discussion</label>
+          <select id="discussions" name="discussions">
+            <option value="1">Discussion 1</option>
+            <option value="2">Discussion 2</option>
+            <option value="3">Discussion 3</option>
+            <option value="4">Discussion 4</option>
+          </select>
+          <br><br>
+    
+          <input id='edit-button' type="submit" name="submit" value="Edit Comment" class="submit">
+        </form>
+      `;
+    }
 }
 
 Comment.all = []
